2c50c798660d8c97dc0ba2fd43fad86a09c8f7e6
Dave Snowdon <dave@davesnowdon.com>
2022-12-27 18:31:22 +0000

    cmake gRPC build working

--- make-desktop.sh
+++ make-desktop.sh
@@ -1,8 +1,3 @@
 #! /bin/sh
-conan profile new default --detect
-conan profile update settings.compiler.libcxx=libstdc++11 default
-cd build-naoqi-sdk
-conan install .. -s build_type=Debug --build missing
-cd .. 
 qibuild configure -c naoqi-sdk body
 qibuild make -c naoqi-sdk body
